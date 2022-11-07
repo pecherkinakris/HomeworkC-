15aabeeae7ee8797b6d28f70f15188296d6b9f54
pecherkinakris <pecerkinakristina@gmail.com>
2022-11-07 20:31:05 +0300

    <Первое число - номер урока, второе число- номер задания>

---
+++ 1.4/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int x = 0;
+
+if (num%2 ==0)
+{
+while (x <= num)
+{
+  Console.WriteLine(x+2);
+  x=x+2;  
+}    
+}
+else{
+    while(x<= num-1)
+{
+   Console.WriteLine(x+2);
+    x=x+2; 
+}
+}
