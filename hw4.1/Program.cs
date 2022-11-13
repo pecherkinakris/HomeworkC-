a675842f3d4706109b8d5b529122455ee41c49fa
pecherkinakris <pecerkinakristina@gmail.com>
2022-11-13 19:33:45 +0300

    4 семинар

---
+++ hw4.1/Program.cs
@@ -0,0 +1,25 @@
+﻿//Напишите цикл, который принимает на вход
+//два числа (A и B) и возводит число A в натуральную
+//степень B.
+Console.WriteLine("Введите два числа");
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+Console.WriteLine(Exp(num1, num2));
+
+
+
+int Exp(int number1, int number2)
+{   
+   int result = 1;
+   for(int i = 0; i < number2; i++)
+   {
+    result = result * number1;
+   }
+    //int i = 0;
+    //while (i < number2)
+    //{
+        //result = result * number1;
+        //i++;
+    //}
+    return result;
+}
---
+++ hw4.2/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая принимает на
+//вход число и выдаёт сумму цифр в числе.
+
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine(SumNum(num));
+
+int SumNum(int number)
+{
+    int result = 0;
+    int digit = 0;
+    while (number > 0)
+    {
+        digit = number % 10;
+        result = result + digit;
+        number = number / 10;
+    }
+return result;
+}
